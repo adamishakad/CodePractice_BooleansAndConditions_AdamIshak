9a9ed38f65cab940878b5c98108515e714d1cb9d
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-06 20:06:59 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Adam Ishak
+// Code Practice Booleans and Conditions
+// 02/07/2024
+namespace CodePractice_BooleansAndConditions_AdamIshak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
